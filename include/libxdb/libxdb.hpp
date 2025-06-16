6d9dca56a8c9e22eff92ebd721335208082ca31d
Apricity <stamp1024@gmail.com>
2025-06-16 23:34:54 +0800

    init

---
+++ include/libxdb/libxdb.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace xdb {
+void say_hello();
+}
---
+++ src/libxdb.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <libxdb/libxdb.hpp>
+
+void xdb::say_hello() { std::cout << "Hello from xdb!" << std::endl; }
