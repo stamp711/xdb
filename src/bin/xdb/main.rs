28265591bb956f198d8158e9ceb9d7567146b9e1
Apricity <stamp1024@gmail.com>
2026-06-17 00:05:12 +0800

    Port rich stop-reason reporting to the debugger shell

--- src/bin/xdb/main.rs
+++ src/bin/xdb/main.rs
@@ -7,7 +7,11 @@ use std::sync::atomic::{AtomicI32, Ordering};
 use nix::sys::signal::{SigHandler, Signal};
 use rustyline::DefaultEditor;
 use rustyline::error::ReadlineError;
-use xdb::{Error, Pid, ProcessState, StopReason, Target};
+use xdb::syscalls::syscall_id_to_name;
+use xdb::{
+    Error, HardwareStop, Pid, Process, ProcessState, StopReason, Stoppoint, SyscallInformation,
+    Target, TrapType,
+};
 
 static INFERIOR_PID: AtomicI32 = AtomicI32::new(0);
 
@@ -120,33 +124,149 @@ fn step(target: &mut Target, op: fn(&mut Target) -> xdb::Result<StopReason>) -> 
     Ok(())
 }
 
-fn handle_stop(target: &Target, reason: StopReason) {
-    let pid = target.process().pid();
-    match reason.state {
-        ProcessState::Stopped => {
+fn format_syscall_trap_info(syscall: &SyscallInformation) -> String {
+    if syscall.is_entry {
+        return " (syscall entry)".to_string();
+    }
+    " (syscall exit)".to_string()
+}
+
+fn print_syscall_details(syscall: &SyscallInformation) {
+    let syscall_name = syscall_id_to_name(syscall.id).unwrap_or("unknown");
+
+    if syscall.is_entry {
+        if let Some(args) = syscall.args {
             println!(
-                "Process {pid} stopped with signal {}",
-                signal_name(reason.info)
+                "syscall entry: {syscall_name}({:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x})",
+                args[0], args[1], args[2], args[3], args[4], args[5]
             );
-            if target.stack().inline_height() > 0 {
-                let die = target.dwarf().die(target.current_frame_of_inline_stack());
-                if let (Ok(file), Ok(line)) = (die.file(), die.line()) {
-                    print_source(&file.path, line, 3);
-                }
-            } else if let Some(entry) = target.line_entry_at_pc()
-                && let Some(file) = target.source_file_at_pc()
+        } else {
+            println!("syscall entry: {syscall_name}(...)");
+        }
+    } else if let Some(ret) = syscall.ret {
+        println!("syscall exit: {syscall_name}(...) = {ret:#x}");
+    } else {
+        println!("syscall exit: {syscall_name}(...)");
+    }
+}
+
+fn get_sigtrap_info(process: &Process, reason: &StopReason) -> String {
+    let mut message = String::new();
+
+    let Some(trap) = reason.trap else {
+        return message;
+    };
+    match trap {
+        TrapType::SoftwareBreakpoint => {
+            if let Ok(pc) = process.get_pc()
+                && let Ok(bp) = process.breakpoint_sites().get_by_address(pc)
             {
-                print_source(&file, entry.line, 3);
+                message = format!(" (breakpoint {})", bp.id());
             }
         }
-        ProcessState::Exited => println!("Process {pid} exited with status {}", reason.info),
-        ProcessState::Terminated => {
-            println!(
-                "Process {pid} terminated with signal {}",
-                signal_name(reason.info)
-            );
+        TrapType::SingleStep => {
+            message = " (single step)".to_string();
         }
-        ProcessState::Running => {}
+        TrapType::HardwareStoppoint => match process.get_current_hardware_stoppoint() {
+            // Hardware breakpoint
+            Ok(HardwareStop::Breakpoint(id)) => {
+                message = format!(" (breakpoint {id})");
+            }
+            // Hardware watchpoint
+            Ok(HardwareStop::Watchpoint(watchpoint_id)) => {
+                message = format!(" (watchpoint {watchpoint_id})");
+                if let Ok(wp) = process.watchpoints().get_by_id(watchpoint_id) {
+                    if wp.data() == wp.previous_data() {
+                        message += &format!("\n Value: {:#x}", wp.data());
+                    } else {
+                        message += &format!("\n Previous Value: {:#x}", wp.previous_data());
+                        message += &format!("\n Current Value: {:#x}", wp.data());
+                    }
+                }
+            }
+            Err(_) => {}
+        },
+        TrapType::Syscall => {
+            if let Some(syscall) = reason.syscall {
+                message = format_syscall_trap_info(&syscall);
+            } else {
+                message = " (syscall)".to_string();
+            }
+        }
+        TrapType::Unknown => {}
+    }
+
+    message
+}
+
+fn generate_signal_stop_reason(target: &Target, reason: &StopReason) -> String {
+    let process = target.process();
+    let Ok(pc) = process.get_pc() else {
+        return format!("stopped by signal {}", signal_name(reason.info));
+    };
+    let mut message = format!(
+        "stopped by signal {}, {:#x}",
+        signal_name(reason.info),
+        pc.addr()
+    );
+
+    let func_name = target.function_name_at_address(pc);
+    if !func_name.is_empty() {
+        message += &format!(" in {func_name} ()");
+    }
+
+    if let Some(entry) = target.line_entry_at_pc()
+        && let Some(file) = target.source_file_at_pc()
+    {
+        let file = file
+            .file_name()
+            .map(|f| f.to_string_lossy().into_owned())
+            .unwrap_or_default();
+        message += &format!(" at {file}:{}", entry.line);
+    }
+
+    if reason.info == Signal::SIGTRAP as u8 {
+        message += &get_sigtrap_info(process, reason);
+    }
+
+    message
+}
+
+fn print_stop_reason(target: &Target, reason: &StopReason) {
+    let message = match reason.state {
+        ProcessState::Running => "is running".to_string(),
+        ProcessState::Stopped => generate_signal_stop_reason(target, reason),
+        ProcessState::Exited => format!("exited with status {}", reason.info),
+        ProcessState::Terminated => format!("terminated by signal {}", signal_name(reason.info)),
+    };
+    println!("Process {} {}", target.process().pid(), message);
+
+    // Print additional syscall details if this is a syscall trap
+    if reason.state == ProcessState::Stopped
+        && reason.info == Signal::SIGTRAP as u8
+        && reason.trap == Some(TrapType::Syscall)
+        && let Some(syscall) = reason.syscall
+    {
+        print_syscall_details(&syscall);
+    }
+}
+
+fn handle_stop(target: &Target, reason: StopReason) {
+    print_stop_reason(target, &reason);
+    if reason.state == ProcessState::Stopped {
+        if target.stack().inline_height() > 0 {
+            let die = target.dwarf().die(target.current_frame_of_inline_stack());
+            if let (Ok(file), Ok(line)) = (die.file(), die.line()) {
+                print_source(&file.path, line, 3);
+            }
+        } else if let Some(entry) = target.line_entry_at_pc()
+            && let Some(file) = target.source_file_at_pc()
+        {
+            print_source(&file, entry.line, 3);
+        } else {
+            let process = target.process();
+            let _ = commands::disassemble::print_disassembly(process, process.get_pc().ok(), 5);
+        }
     }
 }
 
